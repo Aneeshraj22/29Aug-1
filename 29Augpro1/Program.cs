0a0e0073fe8d55e21afb6087a918f649508e6c86
username <username@cognizant.com>
2022-10-20 15:22:35 +0530

    Add project files.

---
+++ 29Augpro1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29Augpro1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            data<string> a= new data<string>();
+
+            a.Push("car");
+            a.Push("auto");
+            a.Push("bike");
+            a.Push("bus");
+            Console.WriteLine("element to be poped");
+            a.Pop();
+            Console.WriteLine("peeked....");
+            a.Peek();
+            Console.WriteLine("print the element currently peeked...");
+            string q=a.Peek();          
+            Console.WriteLine(q);
+            Console.WriteLine("Remaining data.............");
+            a.printingData();
+            Console.ReadLine();
+        }
+    }
+}
